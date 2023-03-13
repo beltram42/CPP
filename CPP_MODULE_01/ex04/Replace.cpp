0b7ba5ad440ef6a1e6a29a6dcdf6c5bfd26ef956
alambert <alambert@student.42.fr>
2023-03-14 00:42:27 +0100

    Module_01 ex04 done

---
+++ CPP_MODULE_01/ex04/Replace.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Replace.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alambert <alambert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/11 15:26:29 by alambert          #+#    #+#             */
+/*   Updated: 2023/03/14 00:37:35 by alambert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Replace.hpp"
+
+// Constructor Destructor *************************************************** //
+Sed::Replace(/* args */) {
+}
+
+Sed::~Replace() {
+}
+// *************************************************** Constructor Destructor //
+
+
+
+// Member functions ********************************************************* //
+
+void    Sed::ft_Replace(std::string file, std::string str1, std::string str2)
+{
+    std::string     str, buff, fileName;
+    std::size_t     found(0);
+    
+    std::ifstream   inFile (file.c_str());
+    if (!inFile) {
+        std::cout << "*** Error, file cannot be open or read" << std::endl;
+        return;
+    }
+    fileName = file += ".replace";
+    std::ofstream outFile(fileName.c_str());
+    if (!outFile) {
+        std::cout << "*** Error, file cannot be created" << std::endl;
+        return;
+    }
+    while (std::getline(inFile, str)) {
+        buff.append(str.append("\n"));
+    } 
+    while ((found = buff.find(str1)) >= 0 && found != std::string::npos) {
+        buff.erase(found, str1.size());
+		buff.insert(found, str2);
+    }
+    inFile.close();
+    outFile << buff;
+    outFile.close();
+}
+// ********************************************************* Member functions //
+
+
+
+// Non Member functions ***************************************************** //
+// ***************************************************** Non Member functions //
--- CPP_MODULE_01/ex04/main.cpp
+++ CPP_MODULE_01/ex04/main.cpp
@@ -6,7 +6,17 @@
 /*   By: alambert <alambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/11 15:26:36 by alambert          #+#    #+#             */
-/*   Updated: 2023/03/11 15:26:37 by alambert         ###   ########.fr       */
+/*   Updated: 2023/03/14 00:41:29 by alambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Replace.hpp"
+
+int	main(int ac, char **av) {
+	if (ac != 4) {
+		std::cout << "*** Error, mandatory format = ./Replace file s1 s2" << std::endl;
+	}
+	Replace newFile;
+    newFile.ft_Replace(av[1], av[2], av[3]);
+	return (0);
+}
