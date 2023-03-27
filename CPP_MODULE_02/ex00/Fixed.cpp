2d51e02b18aaa6e679c39aa83254bae109fbf63c
alambert <alambert@student.42.fr>
2023-03-27 14:40:20 +0200

    02_00 ok

--- CPP_MODULE_02/ex00/Fixed.cpp
+++ CPP_MODULE_02/ex00/Fixed.cpp
@@ -6,7 +6,51 @@
 /*   By: alambert <alambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 12:53:03 by alambert          #+#    #+#             */
-/*   Updated: 2023/03/27 12:53:04 by alambert         ###   ########.fr       */
+/*   Updated: 2023/03/27 14:38:14 by alambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+// Constructor Destructor *************************************************** //
+Fixed::Fixed( void ) : _privateN(0)	{
+	std::cout << "Default constructor called // _privateN set to 0" << std::endl;
+}
+
+Fixed::Fixed(Fixed const & inst)	{
+	std::cout << "Copy constructor called // returns a copy of *this = inst" << std::endl;
+	*this = inst;
+}
+
+Fixed::~Fixed(void)	{
+	std ::cout << "Destructor called (non virtual)" << std::endl;
+}
+// *************************************************** Constructor Destructor //
+
+
+
+// Member functions ********************************************************* //
+Fixed&	Fixed::operator=(Fixed const & rhs)	{
+
+	std::cout << "Copy - Assignment operator overload called // sets _privateN value to one from rhs instance " << std::endl;
+	this->_privateN = rhs.getRawBits();
+	return *this;
+}
+
+void	Fixed::setRawBits(int const raw)	{
+
+	std::cout << "setRawBits - Member function called // sets _privateN to raw" << std::endl;
+	this->_privateN = raw;
+}
+
+int	Fixed::getRawBits(void)  const	{
+
+	std::cout << "getRawBits - Member function called // returns _privateN" << std::endl;
+	return (this->_privateN);
+}
+// ********************************************************* Member functions //
+
+
+
+// Non Member functions ***************************************************** //
+// ***************************************************** Non Member functions //
--- CPP_MODULE_02/ex00/main.cpp
+++ CPP_MODULE_02/ex00/main.cpp
@@ -6,7 +6,22 @@
 /*   By: alambert <alambert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 12:53:12 by alambert          #+#    #+#             */
-/*   Updated: 2023/03/27 12:53:14 by alambert         ###   ########.fr       */
+/*   Updated: 2023/03/27 14:22:58 by alambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int	main(void)	{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return 0;
+}
